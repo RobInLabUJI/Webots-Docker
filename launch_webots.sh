ec8e9edbc0ef43b91b1e5530dc50e11dd2f4c0b2
Enric Cervera <ecervera@uji.es>
2020-03-20 20:42:09 +0100

    add world parameter

--- launch_webots.sh
+++ launch_webots.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <WORLD>" >&2
+  exit 1
+fi
+
 export WEBOTS_HOME=$HOME/webots
 export AUDIODEV=null
 export DEBIAN_FRONTEND=noninteractive
 export DISPLAY=:99
 export LIBGL_ALWAYS_SOFTWARE=true
 Xvfb :99 -screen 0 1024x768x16 &
-cd ~/webots
-./webots --stdout --stderr --batch --no-sandbox --stream
+./webots/webots --stdout --stderr --batch --no-sandbox --stream $1
