15840748eca86b4736546d0e3e8e5eb84518f415
Enric Cervera <ecervera@uji.es>
2020-03-26 18:39:51 +0100

    fix docker parameters

--- launch.sh
+++ launch.sh
@@ -2,14 +2,15 @@
 
 xhost +local:root
 
-docker run --rm \
+docker run --rm -it \
     --gpus all \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -p 1234:1234 \
     -p 8888:8888 \
-    webots-docker
+    webots-docker:8.4 \
+    /bin/bash
 
 xhost -local:root
 
